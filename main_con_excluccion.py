6b2187f3613867cc762e61dec1f20ed627dc4679
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2022-04-30 22:33:12 -0300

    -Add comments

--- main_con_excluccion.py
+++ main_con_excluccion.py
@@ -8,14 +8,26 @@ numero_de_turno = [0, 0, 0]
 
 
 def protocolo_de_entrada(id_hebra):
+    # Proceso entrando
     entrando[id_hebra] = True
+
+    # Calcula el numero del turno
     numero_de_turno[id_hebra] = 1 + max(numero_de_turno)
+
+    # Proceso consiguió turno
     entrando[id_hebra] = False
 
+    # Por cada proceso
     for j in range(len(entrando)):
+        # Si el proceso j está entrando
         while entrando[j]:
             pass
+
+        # Si el proceso j tiene un número de turno Y el número del proceso j es menor al número de turno de este proceso
         while numero_de_turno[j] != 0 and (numero_de_turno[j] < numero_de_turno[id_hebra] or (
+
+                # Si el número de turno del proceso j es igual al número de turno de este proceso
+                # Y el proceso j tiene ID menor al ID de este programa
                 numero_de_turno[j] == numero_de_turno[id_hebra] and j < id_hebra)):
             pass
 
@@ -23,7 +35,6 @@ def protocolo_de_entrada(id_hebra):
 def sec_critica(id_hebra):
     global variable_critica
 
-    print(f"ID: {id_hebra} | START | Variable Critica: {variable_critica} \n", end="")
     time.sleep(random.randint(0, 2))
 
     variable_local = variable_critica
@@ -35,17 +46,21 @@ def sec_critica(id_hebra):
     variable_critica = variable_local
     time.sleep(random.randint(0, 2))
 
-    print(f"ID: {id_hebra} | END | Variable Critica: {variable_critica} \n", end="")
-
 
 def protocolo_de_sailda(id_hebra):
+    # El numero de turno del proceso vuelve a 0
     numero_de_turno[id_hebra] = 0
 
 
 def funcion_a_correr(id_hebra):
+    print(f"ID: {id_hebra} | START | Variable Critica: {variable_critica} \n", end="")
+    time.sleep(random.randint(0, 2))
+
     protocolo_de_entrada(id_hebra)
     sec_critica(id_hebra)
     protocolo_de_sailda(id_hebra)
+
+    print(f"ID: {id_hebra} | END | Variable Critica: {variable_critica} \n", end="")
 
 
 if __name__ == '__main__':
